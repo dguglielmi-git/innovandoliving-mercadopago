1d4ff9254a19e3b53ed3fc860f88738e3c2e0d38
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-03-24 20:00:02 -0300

    chore: replace magic strings by constants

--- services/dbconfig.js
+++ services/dbconfig.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
+const { DB_CONNECTION_SUCCESSFULLY } = require("../utils/constants");
 
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("DB Connection succesfully.");
-    } catch (error) {
-        console.log(error);
-        throw new Error(`Error connecting to DB: ${error}`);
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(DB_CONNECTION_SUCCESSFULLY);
+  } catch (error) {
+    console.log(error);
+    throw new Error(`${ERROR_CONNECTING_DB} ${error}`);
+  }
 };
 
 module.exports = {
-    dbConnection,
+  dbConnection,
 };
