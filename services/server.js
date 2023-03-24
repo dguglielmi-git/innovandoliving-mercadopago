92400a2975c9c9ed1a504af0366ef531ab2d600a
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-03-24 19:15:59 -0300

    chore: updating structure of the project

--- services/server.js
+++ services/server.js
@@ -1,36 +1,36 @@
-const cors = require("cors");
-const express = require("express");
-const { dbConnection } = require("./dbconfig");
+const cors = require('cors');
+const express = require('express');
+const { dbConnection } = require('./dbconfig');
 
 class Server {
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-        this.mercadoPagoPath = "/api/mercadopago";
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.mercadoPagoPath = '/api/mercadopago';
 
-         this.connectDB();
-        this.middlewares();
-        this.routes();
-    }
+    this.connectDB();
+    this.middlewares();
+    this.routes();
+  }
 
-    async connectDB() {
-        await dbConnection();
-    }
+  async connectDB() {
+    await dbConnection();
+  }
 
-    middlewares() {
-        this.app.use(cors());
-        this.app.use(express.json());
-    }
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
 
-    routes() {
-        this.app.use(this.mercadoPagoPath, require("../routes/mpRoute"));
-    }
+  routes() {
+    this.app.use(this.mercadoPagoPath, require('../routes/mpRoute'));
+  }
 
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Server listening on port ${this.port}`);
-        });
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
